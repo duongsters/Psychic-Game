daed14855fe753de4a5124bb21d020cd3df1087d
Andrew Duong <duongsters@gmail.com>
2019-08-31 00:10:16 -0700

    made comments and cleaned up my bracket alignments which will make it much easier for the reader

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Psychic-Game
+
+# Title: Psychic-Game
+
+## Site Picture:
+![My Site](images/Screenshot.png)
+
+## Technologies Used:
+- HTML: Used to create elements 
+- JavaScript: Used in the HTLM files and JS files
+- CSS: Used to make to make the styling of the background image and colors & sizes
+- Git: Used to document the changes of the source code
+- GitHub: Used to create the repository for the html and css in which that can be pushed to GitHub domain
+
+## Summary:
+This assignment we learned to used all the javascript  learned throughout the class and where we were able to fully implement out the DOM methods.
+
+## Code Snippet:
+```html
+    <div class="border" style="color: white">
+
+        <h3>Riddle me this...riddle me that...</h3>
+        <h3>Can you guess what letter I'm thinking of right now?</h3>
+
+                    <br>
+                    <br>
+                    <br>
+<!-- Set a div class that sets rows so the unique IDs within this div row class are in uniform to each other to the output results   -->
+                <div class="row">
+<!-- I specifically chose to split the columns in 2, so I created a specifc   -->
+                    <div class="col-md-6">
+
+                        <ul id="psychicBoard" class="psychicVariables" style="background-image: -webkit-gradient(
+                            linear, left top, left bottom, from(rgba(50,50,50,0.8)),
+                            to(rgba(80,80,80,0.2)), color-stop(.5,#333333)">
+
+                            <li class="psychicRows" id="win">Wins: </li>
+                            <li class="psychicRows" id="loss">Losses:</li>
+                            <li class="psychicRows" id="usedAttempts">Current number of attempts:</li>
+                            <li class="psychicRows" id="remainingAttempts">Remaining number of attempts:</li>
+
+                        </ul>
+                    </div>
+                </div>
+    </div>
+
+  
+```
+
+
+## Author Links:
+[GitHub](https://github.com/duongsters)
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 //---------intial variables-----------------------------------//
 
 
-//setting my variables initally to 0 at the start of the game
+//setting my variables initally to 0 at the start of the game//
 var win = 0;
 var loss = 0;
 var remainingAttempts = 9;
@@ -10,7 +10,7 @@ var userChoice = [];
 
 
 
-//created variable 'choices' that gives an index array of all 26 CHAR letters to be used within the game
+//Declared variable 'choices' that gives an index array of all 26 CHAR letters to be used within the game//
 var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
                'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
                'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
@@ -18,29 +18,31 @@ var choices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
 
 
 
-//----------------------------------------------------------------
 
-//Created a new variable 'computerChoice' that randomly picks from the 'choice' array index
+//Declared a new variable 'computerChoice' that randomly picks from the 'choice' array index before the user makes a keyboard choice//
 var computerChoice = choices[Math.floor(Math.random()*choices.length)];
 
-//Function newRemainingAttempts is created to run the countdown the remaining attempts there are left until the game is over
+//Function newRemainingAttempts is created to run the countdown the remaining attempts there are left until the game is over//
 function newRemainingAttempts() {
-    //line 25 basiclly was made to display the variable "remainingAttempts" the user/player has left by using queryselector to search within the HTML file
+    //line 29 basiclly was made to grab and point out the variable "remainingAttempts" within index.html; a message is outputed stating how many attempts the user/player has left//
     document.querySelector("#remainingAttempts").innerHTML = "Remaining number of attempts: " + remainingAttempts;
 }
 
-//This function usedAttempts is made to when it runs, it will retrieve the result of the user playing the game and convert the array letter chosen in the 'choices' array index and convert it to a string
+//Function usedAttempt is made to when it runs, it will retrieve the result of the user playing the game and convert the array letter chosen in the 'choices' array index and convert it to a string----//
 function usedAttempt() {
-//I directed a text node where it returns out the message of the letter choices the player has attempted so far within the game by concatnating all of the elements within the 'choice' array index
+//Using the .queryselector & .innerhtml methods, I am able to point out to the ID "usedAttempts" from the index.html file while matching with specified message stating the number of attempts the player user has done so far then shows the results of chars they chose; they char choices chosen are then displayed and seperated by commas//
     document.querySelector("#usedAttempts").innerHTML = "Current number of attempts: " + userChoice.join(', ');
 }
+// ----this will basically tell the code to run the newRemainingAttempts function---//
 newRemainingAttempts();
 
 
 
-//Created a new variable 'gameover' that runs a function to when the user has no more attempts, the
+//Created a new variable 'gameover' that runs a function to when the user has no more attempts, the game will eventually restart back to the inital 9 attempts given//
     var gameover = function() {  
+            //gave the variable 'choice' to be an open array where the user could input any char on the keyboard//
             userChoice = [];
+            //the remainAttempts count has remained the same with the 9 attempts given initally for the player//
             remainingAttempts = 9;
             var computerChoice = choices[Math.floor(Math.random() * choices.length)];
 }
@@ -51,22 +53,25 @@ newRemainingAttempts();
 
 //created this event function 
 document.onkeyup = function(event) {
+   //when this onkeyup function event is running, the variable "remainingAttempts" will get clicked '-1' from attempts left//
     remainingAttempts--;
-//created variable userAttempts withing this event function  that will convert any uppercased letter to lowercase
+//created variable userAttempts withing this event function  that will convert any uppercased letter to lowercase//
     var userAttempts = String.fromCharCode(event.keyCode).toLowerCase();
-
+//used the .push method in order to append the array value(s) chosen by the player/user from 'userChoice' then return a new length property of the object 'userAttempts'//
     userChoice.push(userAttempts);
     newRemainingAttempts();
     usedAttempt();
 
-
+//if the user's choices matches with randomized choices of the computer concurrently, the first 'if' command will tell the user has won in the match; then global variable 'wins' will get clicked up with a '+1' increase//
     if (userAttempts === computerChoice) {
         win++;
         document.querySelector("#win").innerHTML = "Number of Wins: " + win;
         gameover();
     }
+    //if the user and computer choices dont match, then the 'else if' conditional statement is waiting in the end to run when it recognizes that the set variable 'remainingAttempts' has reached to null. It would count the current gameplay as a loss and the 'loss' variable clicker will get ticked "+1"//
     else if (remainingAttempts === 0) {
         loss++;
+        //the .queryselector is used to point out to the index.html variable 'loss' then outputs that given information in a message in the browser that tell the amount of losses the user has made so far//
         document.querySelector("#loss").innerHTML = "Number of losses: " + loss;
         gameover();
     }
